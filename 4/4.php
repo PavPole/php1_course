398569ed75eef5dc445d74e5c55070b81cdfc4b0
PavPole <pole123rus@gmail.com>
2021-10-20 17:32:16 +0300

    add

---
+++ 4/4.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+</head>
+<body>
+<?php
+$images = scandir('img');
+foreach ($images as $img) {
+    if(is_file('img/'.$img)){
+
+?>
+
+
+
+<a href='/img/<?php echo $img; ?>' target='_blank'>
+    <img src='/img/<?php echo $img; ?>' style='width:12%'>
+</a> 
+
+<?php
+    }
+}
+?>
+</body>
+
+
+
+</html>
